1a39768f7a4c6a0128573ee88dc6a541065ab2aa
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-10-03 16:09:48 +0300

    Added 'section-3' block

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
-lazyload();
+document.addEventListener("DOMContentLoaded", () => {
+  lazyload();
+
+  let tabListArr = document.querySelectorAll(".tab-list");
+
+  tabListArr.forEach((element) => {
+    element.addEventListener("click", tabHandler);
+  });
+});
+
+function tabHandler(e) {
+  let tabItemArr = this.querySelectorAll(".tab-item");
+  let paneList = this.nextElementSibling;
+  let paneItemArr = paneList.querySelectorAll(".pane-item");
+
+  if (e.target.classList.contains("is-active") || e.target == this) {
+    return;
+  }
+  tabItemArr.forEach((element, index) => {
+    if (element == e.target) {
+      // activate the tab
+      e.target.classList.add("is-active");
+      // activate the pane
+      paneItemArr[index].classList.add("show");
+    } else {
+      // deactivate the tabs
+      element.classList.remove("is-active");
+      // deactivate the panes
+      paneItemArr[index].classList.remove("show");
+    }
+  });
+}
